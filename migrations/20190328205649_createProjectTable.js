6da966016992e785894224322e204726a4310650
Francis Chen <fncis.chen@gmail.com>
2019-03-29 08:45:29 -0700

    added migrations for tables

---
+++ migrations/20190328205649_createProjectTable.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("projects", function (tbl){
+    tbl.increments();
+    tbl.string("name")
+        .unique().notNullable();
+    tbl.text("description");
+    tbl.bool("completed");
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("projects");
+}
---
+++ migrations/20190328205701_createActionTable.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("actions", function (tbl){
+      tbl.increments();
+      tbl.string("name").references("name").inTable("projects")
+      tbl.text("description");
+      tbl.string("notes");
+      tbl.bool("completed");
+  
+    })
+  };
+  
+  exports.down = function(knex, Promise) {
+      return knex.schema.dropTableIfExists("projects");
+  }
+  
