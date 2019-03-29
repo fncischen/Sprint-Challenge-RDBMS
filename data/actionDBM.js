f241e0ffdf9243076096e38ed3050bb122dab56c
Francis Chen <fncis.chen@gmail.com>
2019-03-29 08:56:26 -0700

    set up data model and relevant scripts

---
+++ data/actionDBM.js
@@ -0,0 +1,19 @@
+const db = require("/dbConfig.js");
+
+module.exports = {
+    getActions,
+    getActionId,
+    addAction
+}
+
+function getActions() {
+    return db("actions")
+}
+
+function getActionId(id) {
+    return db("actions").where({"id": id})
+}
+
+function addAction(action) {
+    return db("actions").insert(action);
+}
---
+++ data/projectDBM.js
@@ -0,0 +1,21 @@
+const db = require("/dbConfig.js");
+
+module.exports = {
+    getProjects,
+    getProjectById,
+    insertProject
+}
+
+function getProjects(){
+    return db("projects");
+}
+
+function getProjectById(id){
+    return db("projects")
+    .where({id : id})
+    .join("actions", "actions.name", "=", "projects.name");
+}
+
+function insertProject(project){
+    return db("projects").insert(project);
+}
