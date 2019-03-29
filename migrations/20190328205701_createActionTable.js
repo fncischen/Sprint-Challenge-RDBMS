a53df6c690c6d497bb36828b7b78ad1737fe1b7c
Francis Chen <fncis.chen@gmail.com>
2019-03-29 09:56:55 -0700

    refactored action table

--- migrations/20190328205701_createActionTable.js
+++ migrations/20190328205701_createActionTable.js
@@ -1,7 +1,8 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable("actions", function (tbl){
       tbl.increments();
-      tbl.string("name").references("name").inTable("projects")
+      tbl.string("name").notNull().unique();
+      tbl.string("project_name").references("name").inTable("projects")
       tbl.text("description");
       tbl.string("notes");
       tbl.bool("completed");
