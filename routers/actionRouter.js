418a06765f227f25d46a3e71b541c48eead2adb8
Francis Chen <fncis.chen@gmail.com>
2019-03-29 09:14:06 -0700

    set up routers

---
+++ routers/actionRouter.js
@@ -0,0 +1,21 @@
+const express = require ("express");
+const router = express.Router();
+const actions = require("../data/actionDBM.js");
+
+router.get("/", (res,req) => {
+    actions.getActions()
+    .then(result => res.status(200).json(result))
+    .catch(() => res.status(500).json({errorMessagE: "There was an error in retrieving your data"}));
+})
+
+router.get("/:id", (res,req) => {
+    actions.getActionId(res.params.id)
+    .then(result => res.status(200).json(result))
+    .catch(() => res.status(500).json({errorMessagE: "There was an error in retrieving your data"}));
+})
+
+router.post("/", (res,req) => {
+    actions.addAction(req.body)
+    .then(result => res.status(200).json(result))
+    .catch(() => res.status(500).json({errorMessagE: "There was an error in posting your data"}));
+})
---
+++ routers/projectRouter.js
@@ -0,0 +1,21 @@
+const express = require ("express");
+const router = express.Router();
+const projects = require("../data/projectDBM.js");
+
+router.get("/", (res,req) => {
+    projects.getProjects()
+    .then(result => res.status(200).json(result))
+    .catch(() => res.status(500).json({errorMessagE: "There was an error in retrieving your data"}));
+})
+
+router.get("/:id", (res,req) => {
+    projects.getProjects(res.params.id)
+    .then(result => res.status(200).json(result))
+    .catch(() => res.status(500).json({errorMessagE: "There was an error in retrieving your data"}));
+})
+
+router.post("/", (res,req) => {
+    projects.insertProject(req.body)
+    .then(result => res.status(200).json(result))
+    .catch(() => res.status(500).json({errorMessagE: "There was an error in posting your data"}));
+})
